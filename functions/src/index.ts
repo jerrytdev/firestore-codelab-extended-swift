408f32d62fa4c62f7f4f3c28e2286192b7414598
Jerry Tung <jerry.tung.77@gmail.com>
2018-11-01 13:00:29 -0700

    Step 9.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,10 +14,58 @@
 //
 
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+type Firestore = admin.firestore.Firestore
+const app = admin.initializeApp();
 
-// TODO(DEVELOPER): Write the computeAverageReview Function here.
+  // TODO(DEVELOPER): Write the computeAverageReview Function here.
+export const computeAverageReview = functions.firestore
+  .document('reviews/{reviewId}').onWrite((change, context) => {
+  // get the data from the write event
+  const eventData = change.after.data();
+  // get the previous value, if it exists
+  const prev = change.before;
+  const rating = eventData.rating;
+  let previousValue
+  if (prev.exists) {
+    previousValue = prev.data();
+    const prevRating = previousValue.rating;
+    if (rating === prevRating) {
+      console.log("not a new rating");
+    return null;
+  	}
+  }
+  const restaurantID = eventData.restaurantID;
+  const db = app.firestore()
+  if (prev.exists) {
+    const difference = previousValue.rating - rating
+    return updateAverage(db, restaurantID, difference, true)
+  } else {
+    return updateAverage(db, restaurantID, rating, false)
+  }
+});
 
 // TODO(DEVELOPER): Add updateAverage helper function here.
+async function updateAverage(db: Firestore, restaurantID: string, newRating: number, prev: boolean) {
+  const updateDB = db.collection('restaurants').doc(restaurantID);
+  const restaurantDoc = await updateDB.get();
+  if (!restaurantDoc.exists) {
+    console.log("Document does not exist!");
+    return null;
+  }
+  const oldRating = restaurantDoc.data().averageRating;
+  const oldNumReviews = restaurantDoc.data().reviewCount;
+  let newNumReviews = oldNumReviews + 1;
+  let newAvgRating = ((oldRating*oldNumReviews)+newRating)/newNumReviews;
+  if (prev) {
+    newNumReviews = oldNumReviews;
+    newAvgRating = ((oldRating*oldNumReviews)-newRating)/oldNumReviews;
+  }
+  await updateDB.update({averageRating: newAvgRating, reviewCount: newNumReviews});
+  console.log("average updated");
+  return null;
+}
 
 // TODO(DEVELOPER): Write the updateRest Function here.
 
